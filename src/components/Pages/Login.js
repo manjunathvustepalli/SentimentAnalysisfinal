1566ea61161ce4a6ee9b24e3506ee5150fe6a42b
manjunathvustepalli <manjunathvustepalli@users.noreply.github.com>
2020-10-20 20:30:23 +0530

    login api added

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -1,116 +1,192 @@
-import React from 'react'
-import bg from '../../imgs/bg.png'
-import { Grid, Card, Avatar, Typography, TextField, Button } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import LockRoundedIcon from '@material-ui/icons/LockRounded';
-import green from '@material-ui/core/colors/green';
-import { Link } from 'react-router-dom';
-import { Label } from '@material-ui/icons';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import bg from "../../imgs/bg.png";
+import {
+  Grid,
+  Card,
+  Avatar,
+  Typography,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import LockRoundedIcon from "@material-ui/icons/LockRounded";
+import green from "@material-ui/core/colors/green";
+import { Link } from "react-router-dom";
+import { Label } from "@material-ui/icons";
 
 const styles = {
-    background:{
-        width:'100vw',
-        height:'100vh',
-        backgroundImage:`url(${bg})`,
-        backgroundSize:'cover'
-    },
-    wrapper:{
-        height:'100vh',
-        display:'grid',
-        placeItems:'center',
-    },
-    inputWrapper:{
-        borderBottom:'2px solid #aaa',
-        position:'relative',
-        width:'100%',
-        height:'20px',
-        margin:'20px 0',
-        padding:'10px 0',
-    },
-    inputIcon:{
-        position:'absolute',
-        left:'30px',
-        color:'#aaa'
-    }
-}
+  background: {
+    width: "100vw",
+    height: "100vh",
+    backgroundImage: `url(${bg})`,
+    backgroundSize: "cover",
+  },
+  wrapper: {
+    height: "100vh",
+    display: "grid",
+    placeItems: "center",
+  },
+  inputWrapper: {
+    borderBottom: "2px solid #aaa",
+    position: "relative",
+    width: "100%",
+    height: "20px",
+    margin: "20px 0",
+    padding: "10px 0",
+  },
+  inputIcon: {
+    position: "absolute",
+    left: "30px",
+    color: "#aaa",
+  },
+};
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      '& > *': {
-        margin: theme.spacing(1),
-      },
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
     },
-    large: {
-      width: theme.spacing(15),
-      height: theme.spacing(15),
-      marginBottom:'20px'
+  },
+  large: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    marginBottom: "20px",
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  button: {
+    margin: theme.spacing(1),
+    padding: "15px 0",
+    color: "white",
+    display: "block",
+    textAlign: "center",
+    backgroundColor: `rgb(67,176,42)`,
+    "&:hover": {
+      backgroundColor: `rgb(67,176,42)`,
     },
-    margin: {
-        margin: theme.spacing(1),
-      },
-    button:{
-        margin: theme.spacing(1),
-        padding:'15px 0',
-        color:'white',
-        display:'block',
-        textAlign:'center',
-        backgroundColor:`rgb(67,176,42)`,
-        '&:hover': {
-            backgroundColor:`rgb(67,176,42)`,
-        }
-    }
-  }));
+  },
+}));
 
-function Login() {
-    const classes = useStyles()
-    return (
-        <div style={styles.background}>
-            <Grid container>
-                <Grid item xs={false} sm={false} md={6} lg={6}> 
-                </Grid>
-                <Grid item xs={12} sm={12} md={6} lg={6}>
-                    <div style={styles.wrapper}>
-                    <Card style={{minHeight:'400px',padding:'30px',display:'flex',flexDirection:'column',alignItems:'center'}} >
-                        <Avatar src={require('../../imgs/logo.png')} className={classes.large} />
-                        <Typography variant='h6'>
-                            SOCIAL MEDIA SENTIMENT ANALYSIS
-                        </Typography>
-                        <Typography variant='h6' style={{color:'orange'}}>
-                            FOR GOVT. OF BANGLADESH
-                        </Typography>
-                            <label htmlfor="userName-input" style={{display:'block',width:'100%'}} >
-                            <div style={styles.inputWrapper}>
-                                <AccountCircle style={styles.inputIcon}/>
-                                <input type='text'  placeholder="Enter User Id" id="userName-input" style={{width:'100%',height:'25px',border:'none',outline:'none',textAlign:'center'}} />
-                            </div>
-                            </label>
-                            <label htmlfor="password-input" style={{display:'block',width:'100%'}}>
-                                <div style={styles.inputWrapper}>
-                                    <LockRoundedIcon style={styles.inputIcon} />
-                                    <input type='password' placeholder="Enter Password" id="password-input" style={{width:'100%',height:'25px',border:'none',outline:'none',textAlign:'center'}} />
-                                </div>
-                            </label>
-                        <div>
-                            
-                        </div>
-                        <Button
-                            className={classes.button}
-                            variant="contained"
-                            style={{margin:"10px"}}
-                            component={Link}
-                            to="/summary-dashboard"
-                            fullWidth
-                            >
-                            Enter                                                                       
-                        </Button>
-                    </Card>
-                    </div>
-                </Grid>
-            </Grid>
-        </div>
+function Login(props) {
+  const classes = useStyles();
+  const [Username, setUsername] = useState();
+  const [Password, setPassword] = useState();
+  const Signin = () => {
+    let data = new FormData();
+
+    var formdata = new FormData();
+    formdata.append("username", Username);
+    formdata.append("password", Password);
+
+    var requestOptions = {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://cors-anywhere.herokuapp.com/http://3.7.187.244:9100/auth/login/",
+      requestOptions
     )
+      .then((response) => {if(response.status){
+  props.history.push({
+         pathname: "/summary-dashboard",
+       });}
+      })
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+
+     
+  };
+  return (
+    <div style={styles.background}>
+      <Grid container>
+        <Grid item xs={false} sm={false} md={6} lg={6}></Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          <div style={styles.wrapper}>
+            <Card
+              style={{
+                minHeight: "400px",
+                padding: "30px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Avatar
+                src={require("../../imgs/logo.png")}
+                className={classes.large}
+              />
+              <Typography variant="h6">
+                SOCIAL MEDIA SENTIMENT ANALYSIS
+              </Typography>
+              <Typography variant="h6" style={{ color: "orange" }}>
+                FOR GOVT. OF BANGLADESH
+              </Typography>
+              <label
+                htmlfor="userName-input"
+                style={{ display: "block", width: "100%" }}
+              >
+                <div style={styles.inputWrapper}>
+                  <AccountCircle style={styles.inputIcon} />
+                  <input
+                    type="text"
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Enter User Id"
+                    id="userName-input"
+                    style={{
+                      width: "100%",
+                      height: "25px",
+                      border: "none",
+                      outline: "none",
+                      textAlign: "center",
+                    }}
+                  />
+                </div>
+              </label>
+              <label
+                htmlfor="password-input"
+                style={{ display: "block", width: "100%" }}
+              >
+                <div style={styles.inputWrapper}>
+                  <LockRoundedIcon style={styles.inputIcon} />
+                  <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Enter Password"
+                    id="password-input"
+                    style={{
+                      width: "100%",
+                      height: "25px",
+                      border: "none",
+                      outline: "none",
+                      textAlign: "center",
+                    }}
+                  />
+                </div>
+              </label>
+              <div></div>
+              <Button
+                onClick={Signin}
+                className={classes.button}
+                variant="contained"
+                style={{ margin: "10px" }}
+                component={Link}
+                // to=""
+                fullWidth
+              >
+                Enter
+              </Button>
+            </Card>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
